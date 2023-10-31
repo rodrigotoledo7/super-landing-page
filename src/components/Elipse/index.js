00930f763c10017018e6dfee3348595c7749aeec
Rodrigo Toledo <rodrigotoledo7@gmail.com>
2023-10-30 21:32:07 -0400

    arquivos iniciais do treinamento nextjs

---
+++ src/components/Elipse/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import styles from './styles.css';
+
+export default function Elipse(){
+    return(
+        <>
+         <h1>Elipse</h1> 
+         
+        </>
+    )
+}
---
+++ src/pages/Home/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+// Minhas importações
+import styles from './styles.css';
+import Elipse from '../../components/Elipse';
+
+export default function Home(){
+  return (
+    <>
+      <h1>Home</h1>
+      <Elipse />
+    </>
+  )
+}
+
+
